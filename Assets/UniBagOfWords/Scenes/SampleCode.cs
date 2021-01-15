78629d6c5c4333df379bd90dd361af1c7f7fab31
hanachiru <tomoe.66.tomoe@gmail.com>
2021-01-16 01:06:34 +0900

    サンプルコード実装

---
+++ Assets/UniBagOfWords/Scenes/SampleCode.cs
@@ -0,0 +1,34 @@
+﻿using UniBagOfWords;
+using UnityEngine;
+
+public class SampleCode : MonoBehaviour
+{
+    async void Start()
+    {
+        // テキストデータセット
+        string[] sentences = new[]
+        {
+            "私はラーメンが好きです。",
+            "私は餃子が好きです。",
+            "私はラーメンが嫌いです。"
+        };
+
+        // 1. まずは単語辞書を作る必要があります
+        Vocabulary vocabulary = await Vocabulary.Create(sentences);
+
+        // 2. 先程生成した単語辞書を使ったBoWベクトル変換器を作ります
+        BagOfWordsConverter converter = new BagOfWordsConverter(vocabulary);
+
+        // 3. 好きな文章をBoW変換します
+        int[] bowVec = await converter.ConvertAsync("私はラーメンが嫌いです。");
+
+        // BoWベクトル（テキスト中に単語が出現した回数を並べたもの）
+        // 1,1,1,1,0,1,1,0,1
+        Debug.Log(string.Join(",", bowVec));
+
+        // 単語辞書の単語と順番
+        // 私,は,ラーメン,が,好き,です,。,餃子,嫌い
+        Debug.Log(string.Join(",", vocabulary.Words));
+    }
+}
+
---
+++ Assets/UniBagOfWords/Scenes/SampleCode2.cs
@@ -0,0 +1,37 @@
+﻿using UniBagOfWords;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleCode2 : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private InputField _inputField;
+
+    private BagOfWordsConverter _converter;
+
+    private async void Start()
+    {
+        // テキストデータセット
+        string[] sentences = new[]
+        {
+            "私はラーメンが好きです。",
+            "私は餃子が好きです。",
+            "私はラーメンが嫌いです。"
+        };
+
+        // 1. 単語辞書を作成
+        Vocabulary vocabraryu = await Vocabulary.Create(sentences);
+
+        // 2. 生成した単語辞書を使ったBoWベクトル変換器を作成
+        _converter = new BagOfWordsConverter(vocabraryu);
+    }
+
+    public async void Execute()
+    {
+        // 3. 好きな文章をBoW変換します
+        int[] bowVec = await _converter.ConvertAsync(_inputField.text);
+
+        Debug.Log("[" + string.Join(",", bowVec) + "]");
+        _text.text = "[" + string.Join(",", bowVec) + "]";
+    }
+}
